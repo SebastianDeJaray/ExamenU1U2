a974424c45244078f10a5ad43c34ba17dda2e0d7
SebastianDeJaray <143725068+SebastianDeJaray@users.noreply.github.com>
2023-09-19 08:44:10 -0600

    Add files via upload

---
+++ ExamenU1U2.java
@@ -0,0 +1,37 @@
+
+package examenu1u2;
+ import java.util.Scanner;
+
+
+public class ExamenU1U2 {
+    
+
+    public static void main(String[] args) {
+        
+      Scanner scanner=new Scanner(System.in);  
+        System.out.println("Tienda los TICS ");
+        System.out.println("Primer videojuego");
+        String nom1=scanner.nextLine();
+        System.out.println("Ingresa el precio"+ nom1);
+        double = scanner.nextDouble();
+        
+        System.out.println("Segundo videojuego");
+        String nom2=scanner.nextLine();
+        System.out.println("Ingresa el precio"+ nom2);
+        double = scanner.nextDouble();
+        
+        System.out.println("Tercer videojuego");
+        String nom3=scanner.nextLine();
+        System.out.println("Ingresa el precio"+ nom3);
+        double = scanner.nextDouble();
+        
+        double porcentaje1 = (nom1);
+        
+        
+        
+        
+        
+      
+    }
+    
+}
